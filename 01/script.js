6943b4ebdb90a3e05084a0088a73b0a8e4119223
Anna Sobko <so6ko@icloud.com>
2024-04-28 19:48:07 +0200

    firstscript

---
+++ 01/script.js
@@ -0,0 +1,11 @@
+var a = 'Hello';
+
+let number = 505;
+let userName = "Anna";
+
+const family = 'Anna, Svitlana, Igor, Lev';
+
+alert (a);
+console.log (number);
+console.log (userName);
+console.log (family);
