f50f1bd3b88a573669b105fba7e9632ab7e8838a
Anna Sobko <so6ko@icloud.com>
2024-04-29 13:58:35 +0200

    two

---
+++ 02/script.js
@@ -0,0 +1,36 @@
+let result1 = (5 + 3) * 2 - 7;
+
+let result2 = (10 / 2) ** 2;
+
+let result3 = 2 * (4 + 3) - (8 / 2);
+
+let result4 = (15 % 4) + 5;
+
+let result5 = 3 - (2 ** 4);
+
+console.log (` result1 : (5 + 3) * 2 - 7 = ${result1}`);
+console.log (` result2 : (10 / 2) ** 2 = ${result2}`);
+console.log (` result3 : 2 * (4 + 3) - (8 / 2) = ${result3}`);
+console.log (` result4 : (15 % 4) + 5 = ${result4}`);
+console.log (` result5 : 3 - (2 ** 4) = ${result5}`);
+
+
+let a = 24;
+let b = 6;
+
+let resultA = a + b;
+let resultB = a - b;
+let resultC = a / b;
+let resultD = a * b;
+let resultE = a % b;
+let resultF = a ** b;
+
+console.log (` a = ${a}`);
+console.log (` b = ${b}`);
+
+console.log (` result a + b =  ${resultA}`);
+console.log (` result a - b =  ${resultB}`);
+console.log (` result a / b =  ${resultC}`);
+console.log (` result a * b =  ${resultD}`);
+console.log (` result a % b =  ${resultE}`);
+console.log (` result a ** b =  ${resultF}`);
